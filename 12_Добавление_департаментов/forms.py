8a35f03a5d1771d5baeddd4a30c886a252a992ee
gg817185-cyber <gg817185@gmail.com>
2026-05-18 23:57:40 +0300

    Added 12 task: "Добавление департаментов"

---
+++ 12_Добавление_департаментов/forms.py
@@ -0,0 +1,41 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, IntegerField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import EmailField
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password', message='Пароли не совпадают')])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    position = StringField('Position', validators=[DataRequired()])
+    speciality = StringField('Speciality', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class JobsForm(FlaskForm):
+    job = TextAreaField('Название работы', validators=[DataRequired()])
+    team_leader = IntegerField('Id капитана', validators=[DataRequired()])
+    work_size = IntegerField('Объём работы в часах', validators=[DataRequired()])
+    collaborators = StringField('Участники', validators=[DataRequired()])
+    is_finished = BooleanField('Завершена?')
+    submit = SubmitField('Применить')
+
+
+class DepartmentForm(FlaskForm):
+    title = StringField('Название департамента', validators=[DataRequired()])
+    chief = IntegerField('Id руководителя', validators=[DataRequired()])
+    members = StringField('Участники', validators=[DataRequired()])
+    email = StringField('Email департамента', validators=[DataRequired(), Email()])
+    submit = SubmitField('Применить')
